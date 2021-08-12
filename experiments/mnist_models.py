cc4fd714d455efb17b517c7d3f99d44451a02c33
Andrew Wood <aew61@case.edu>
2021-08-12 19:53:39 -0400

    timings

--- experiments/mnist_models.py
+++ experiments/mnist_models.py
@@ -9,7 +9,7 @@ import torch as pt
 import torch.nn.functional as F
 
 
-class Model_1_5Gb(pt.nn.Module):
+class Model_2Conv2FC(pt.nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1 = pt.nn.Conv2d(1, 10, kernel_size=5)
@@ -44,127 +44,7 @@ class Model_1_5Gb(pt.nn.Module):
                     .reshape(P.size()))
                 param_idx += num_params
 
-class Model_50Gb(pt.nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.fc1 = pt.nn.Linear(28*28, 100)
-        self.fc2 = pt.nn.Linear(100, 10)
-
-    def forward(self,
-                x: pt.Tensor):
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return F.log_softmax(x, -1)
-
-    def get_params(self) -> np.ndarray:
-        params_list: List[np.ndarray] = list()
-        for P in self.parameters():
-            params_list.append(P.cpu().detach().numpy().reshape(-1))
-        return np.hstack(params_list).reshape(-1,1)
-
-    def set_params(self,
-                   theta: np.ndarray) -> None:
-        param_idx: int = 0
-        for P in self.parameters():
-            if len(P.size() > 0):
-                num_params: int = np.prod(P.size())
-                P.copy_(theta[param_idx:param_idx+num_params]
-                    .reshape(P.size()))
-                param_idx += num_params
-
-
-class Model_113Gb(pt.nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.fc1 = pt.nn.Linear(28*28, 150)
-        self.fc2 = pt.nn.Linear(150, 10)
-
-    def forward(self,
-                x: pt.Tensor):
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return F.log_softmax(x, -1)
-
-    def get_params(self) -> np.ndarray:
-        params_list: List[np.ndarray] = list()
-        for P in self.parameters():
-            params_list.append(P.cpu().detach().numpy().reshape(-1))
-        return np.hstack(params_list).reshape(-1,1)
-
-    def set_params(self,
-                   theta: np.ndarray) -> None:
-        param_idx: int = 0
-        for P in self.parameters():
-            if len(P.size() > 0):
-                num_params: int = np.prod(P.size())
-                P.copy_(theta[param_idx:param_idx+num_params]
-                    .reshape(P.size()))
-                param_idx += num_params
-
-
-class Model_317Gb(pt.nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.fc1 = pt.nn.Linear(28*28, 200)
-        self.fc2 = pt.nn.Linear(200, 200)
-        self.fc3 = pt.nn.Linear(200, 10)
-
-    def forward(self,
-                x: pt.Tensor):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return F.log_softmax(x, -1)
-
-    def get_params(self) -> np.ndarray:
-        params_list: List[np.ndarray] = list()
-        for P in self.parameters():
-            params_list.append(P.cpu().detach().numpy().reshape(-1))
-        return np.hstack(params_list).reshape(-1,1)
-
-    def set_params(self,
-                   theta: np.ndarray) -> None:
-        param_idx: int = 0
-        for P in self.parameters():
-            if len(P.size() > 0):
-                num_params: int = np.prod(P.size())
-                P.copy_(theta[param_idx:param_idx+num_params]
-                    .reshape(P.size()))
-                param_idx += num_params
-
-
-class Model_700Gb(pt.nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.fc1 = pt.nn.Linear(28*28, 200)
-        self.fc2 = pt.nn.Linear(200, 200)
-        self.fc3 = pt.nn.Linear(200, 10)
-
-    def forward(self,
-                x: pt.Tensor):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return F.log_softmax(x, -1)
-
-    def get_params(self) -> np.ndarray:
-        params_list: List[np.ndarray] = list()
-        for P in self.parameters():
-            params_list.append(P.cpu().detach().numpy().reshape(-1))
-        return np.hstack(params_list).reshape(-1,1)
-
-    def set_params(self,
-                   theta: np.ndarray) -> None:
-        param_idx: int = 0
-        for P in self.parameters():
-            if len(P.size() > 0):
-                num_params: int = np.prod(P.size())
-                P.copy_(theta[param_idx:param_idx+num_params]
-                    .reshape(P.size()))
-                param_idx += num_params
-
-
-class Model_5Tb(pt.nn.Module):
+class Model_2FC(pt.nn.Module):
     def __init__(self):
         super().__init__()
         self.fc1 = pt.nn.Linear(28*28, 1000)
@@ -174,6 +54,69 @@ class Model_5Tb(pt.nn.Module):
                 x: pt.Tensor):
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
+        return F.log_softmax(x, -1)
+
+    def get_params(self) -> np.ndarray:
+        params_list: List[np.ndarray] = list()
+        for P in self.parameters():
+            params_list.append(P.cpu().detach().numpy().reshape(-1))
+        return np.hstack(params_list).reshape(-1,1)
+
+    def set_params(self,
+                   theta: np.ndarray) -> None:
+        param_idx: int = 0
+        for P in self.parameters():
+            if len(P.size() > 0):
+                num_params: int = np.prod(P.size())
+                P.copy_(theta[param_idx:param_idx+num_params]
+                    .reshape(P.size()))
+                param_idx += num_params
+
+
+class Model_3FC(pt.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = pt.nn.Linear(28*28, 1000)
+        self.fc2 = pt.nn.Linear(1000, 500)
+        self.fc3 = pt.nn.Linear(500, 10)
+
+    def forward(self,
+                x: pt.Tensor):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return F.log_softmax(x, -1)
+
+    def get_params(self) -> np.ndarray:
+        params_list: List[np.ndarray] = list()
+        for P in self.parameters():
+            params_list.append(P.cpu().detach().numpy().reshape(-1))
+        return np.hstack(params_list).reshape(-1,1)
+
+    def set_params(self,
+                   theta: np.ndarray) -> None:
+        param_idx: int = 0
+        for P in self.parameters():
+            if len(P.size() > 0):
+                num_params: int = np.prod(P.size())
+                P.copy_(theta[param_idx:param_idx+num_params]
+                    .reshape(P.size()))
+                param_idx += num_params
+
+class Model_3FC(pt.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = pt.nn.Linear(28*28, 1000)
+        self.fc2 = pt.nn.Linear(1000, 1000)
+        self.fc3 = pt.nn.Linear(1000, 1000)
+        self.fc4 = pt.nn.Linear(1000, 10)
+
+    def forward(self,
+                x: pt.Tensor):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
         return F.log_softmax(x, -1)
 
     def get_params(self) -> np.ndarray:
